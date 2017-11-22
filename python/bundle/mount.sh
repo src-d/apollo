cfb70f8efe2f4237d83ff17d38bbc0dee5e26b92
Vadim Markovtsev <vadim@sourced.tech>
2017-11-22 11:56:32 +0100

    Remove the local file

--- python/bundle/mount.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mount -o bind ~/Projects/sourced.ml $(dirname "$0")/ml
-mount -o bind ~/Projects/spark-2.2.0-bin-hadoop2.7 $(dirname "$0")/spark
-mount -o bind ~/Projects/sourced-engine $(dirname "$0")/engine
