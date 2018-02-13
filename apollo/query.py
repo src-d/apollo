ba3017732bbdd446d4f74e1e60b5afe575d4070a
Alexander Bezzubov <bzz@apache.org>
2018-02-13 12:06:00 +0100

    Add Gitlab URL ouput formating support
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- apollo/query.py
+++ apollo/query.py
@@ -80,7 +80,7 @@ def weighted_jaccard(vec1, vec2):
 
 
 def format_url(repo, commit, path):
-    if repo.startswith("github.com"):
+    if repo.startswith("github.com") or repo.startswith("gitlab.com"):
         return "https://%s/blob/%s/%s" % (repo, commit, path)
     if repo.startswith("bitbucket.org"):
         return "https://%s/src/%s/%s" % (repo, commit, path)
